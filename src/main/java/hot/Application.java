248d339027397f80e41a7e5bb8243735aa8d2779
Your Name <andreykapas@gmail.com>
2015-08-24 12:59:50 +0300

    zazaz

---
+++ src/main/java/hot/Application.java
@@ -0,0 +1,12 @@
+package hot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/hot/Model/Dish.java
@@ -0,0 +1,113 @@
+package Model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.util.Date;
+
+/**
+ * Created by vlad on 10.8.15.
+ */
+
+public class Dish implements Comparable<Dish> {
+
+    private int id;
+
+    private int category_id;
+
+    private String categoryName;
+
+    private int supplier_id;
+
+    private String name;
+
+    private int price;
+
+    private String dateBegin;
+
+    private String dateEnd;
+
+    public Dish(int id, String categoryName, String name, int price, String dateBegin, String dateEnd)
+    {
+        this.id = id;
+        this.categoryName = categoryName;
+        this.name = name;
+        this.price = price;
+        this.dateBegin = dateBegin;
+        this.dateEnd = dateEnd;
+    }
+
+    @Override
+    public int compareTo(Dish dish)
+    {
+        int c1 = this.categoryName.compareTo(dish.getCategoryName());
+        return c1 == 0 ? this.name.compareTo(dish.getName()) : c1;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(int category_id) {
+        this.category_id = category_id;
+    }
+
+    public int getSupplier_id() {
+        return supplier_id;
+    }
+
+    public void setSupplier_id(int supplier_id) {
+        this.supplier_id = supplier_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    @JsonIgnore
+    public String getDateBegin() {
+        return dateBegin;
+    }
+
+    @JsonIgnore
+    public void setDateBegin(String dateBegin) {
+        this.dateBegin = dateBegin;
+    }
+
+    @JsonIgnore
+    public String getDateEnd() {
+        return dateEnd;
+    }
+
+    @JsonIgnore
+    public void setDateEnd(String dateEnd) {
+        this.dateEnd = dateEnd;
+    }
+}
